c5cea9a11d256db9efe6c446e710d4e3dc202000
Hadi Gusty <43689177+HadiGusty@users.noreply.github.com>
2018-10-15 22:23:36 +0700

    Create index.html for PandaClip Github Pages

---
+++ script.js
@@ -0,0 +1,17 @@
+function functionMain() {
+    var x = document.getElementById("main");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function functionSide() {
+    var y = document.getElementById("side");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
